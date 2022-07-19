d66a6d1dd3781955761b6fdb9faa9cc378cdab5e
mananaggarwal2001 <mananaggarwal2001@gmail.com>
2022-07-19 23:20:32 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path= require('path');
+const app = express();
+const port = 80;
+
+// Express related Stuffs
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+// Pug Specific Engine work
+app.set('view engine', 'pug'); // For rendering the template engine and to serve static files on the browser etc.
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Express related APIs
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+
+// Port to Listen the request and serve to the client browser.
+app.listen(port, ()=>{
+    console.log("Server started successfully on the port 80 and the presentation is ready to shown on the Browser.");
+})
